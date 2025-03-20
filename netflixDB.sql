237fef6318287397228441c0bf8ce89de1d60e7c
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2025-03-20 09:42:24 -0500

    Analysing the data I have

--- netflixDB.sql
+++ netflixDB.sql
@@ -13,3 +13,23 @@ CREATE TABLE netflix(
 	listed_in VARCHAR(100),	
 	description VARCHAR(250)
 );
+
+SELECT * FROM 	netflix;
+-- total columns
+SELECT 
+	COUNT(*) AS total_content
+FROM netflix;
+-- total columns in type
+SELECT 
+	COUNT(type) AS  total_type
+FROM netflix;
+
+--Distinct values of column
+SELECT 
+	DISTINCT type
+FROM netflix;
+
+--NULL VALUES OF A COLUMNS
+SELECT casts, title
+FROM netflix
+WHERE casts IS NULL;
